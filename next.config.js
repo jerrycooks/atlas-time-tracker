a8e2b967e9e61a69b92ba6a4aa05ff215963171b
jerrycooks <hello@jarah.xyz>
2025-02-04 22:19:19 -0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+  },
+};
