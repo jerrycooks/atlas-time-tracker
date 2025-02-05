525ac245fc8bc1e2ad6ec84247a6e50399f38629
jerrycooks <hello@jarah.xyz>
2025-02-04 22:40:38 -0800

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
-    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
-  },
-};
